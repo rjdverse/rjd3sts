2c70425f215fbef1594f923b6610ea563568c94f
palatej <palatejean@gmail.com>
2026-03-31 18:27:36 +0200

    Raw ssf

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,15 @@
 #' @importFrom stats frequency is.ts pt start ts
 #' @importFrom rjd3toolkit result dictionary
 NULL
+
+.jd2r_diffuse_likelihood<-function(jrslt){
+    return (structure(list(
+        nobs=.jcall(jrslt, "I", "dim"),
+        ndiffuse=.jcall(jrslt, "I", "getD"),
+        ll=.jcall(jrslt, "D", "logLikelihood"),
+        ssq=.jcall(jrslt, "D", "ssq"),
+        ldet=.jcall(jrslt, "D", "logDeterminant"),
+        dcorr=.jcall(jrslt, "D", "getDiffuseCorrection")
+    ),
+    class = "JD3DIFFUSELIKELIHOOD"))
+}
